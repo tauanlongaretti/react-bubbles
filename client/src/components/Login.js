12d70734b0fe845403931b8fca18e4d1943bd686
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-21 21:26:41 -0500

    Login component created

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,58 @@
 import React from "react";
 
-const Login = () => {
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+  };
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+  render () {
+    return (
+      <div>
+        <h2>Welcome To Bubbles!</h2>
+        <form onSubmit={this.login}>
+          <div>
+            <input 
+              className="username-input"
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              className="password-input"
+              type="text"
+              name="password"
+              placeholder="Password"
+              value={this.state.credentials.password}
+              onChange={this.handleChange} 
+            />
+          </div>
+          <button className="login-button">Log in</button>
+        </form>
+      </div>
+    );
+  };
+}
 
 export default Login;
